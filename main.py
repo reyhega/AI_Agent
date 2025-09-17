75540ba6993d38f1311a5860327b37d10b28ab5a
reyhega <reyhega@gmail.com>
2025-09-16 21:47:24 -0600

    Ch1 - L6: Verbose

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
-
+import sys
 import os
 from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
 load_dotenv()
+
 api_key = os.environ.get("GEMINI_API_KEY")
-
-from google import genai
-
 client = genai.Client(api_key=api_key)
+user_prompt = sys.argv[1]
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+verbose_flag = len(sys.argv) > 2 and sys.argv[2] == "--verbose"
 
+if not sys.argv[1]:
+    print("Please provide prompt")
+    sys.exit(1)
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model="gemini-2.0-flash-001",
+    contents=messages,
 )
+
 print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if verbose_flag:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 def main():
