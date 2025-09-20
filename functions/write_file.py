6de452206a357f0ef75dc4717152a4ec91160058
reyhega <reyhega@gmail.com>
2025-09-19 21:31:15 -0600

    CH2 - L4: Write File setup

---
+++ functions/write_file.py
@@ -0,0 +1,8 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
