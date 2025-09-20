c5c829f2fef7384bca3a44620972c01d5a506b07
reyhega <reyhega@gmail.com>
2025-09-19 23:57:09 -0600

    CH2 - L5: Run Python / Fixes

--- functions/write_file.py
+++ functions/write_file.py
@@ -6,12 +6,15 @@ def write_file(working_directory, file_path, content):
 
     if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
-    
+    if not os.path.exists(abs_file_path):
+        try:
+            os.makedirs(os.path.dirname(abs_file_path), exist_ok=True)
+        except Exception as e:
+            return f"Error: creating directory: {e}"
+    if os.path.exists(abs_file_path) and os.path.isdir(abs_file_path):
+        return f'Error: "{file_path}" is a directory, not a file' 
+       
     try:
-        
-        if not os.path.exists(os.path.dirname(abs_file_path)):
-            os.makedirs(os.path.dirname(abs_file_path))
-        
         with open(abs_file_path, "w") as f:
             f.write(content)
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 def test_get_file_info():
     dirs = (".", "pkg", "/bin", "../")
@@ -24,11 +25,26 @@ def test_write_file():
         print(f'Writing "{case[1]}" to file "{case[0]}"')
         print(write_file("calculator", case[0], case[1]))
 
+def test_run_python_file():
+    test_cases =  [
+        ("main.py", ),
+        ("main.py", ["3 + 5"]),
+        ("tests.py", ),
+        ("../main.py", ),
+        ("nonexistent.py", )
+    ]
+    for case in test_cases:
+        print(f'Result for "{case[0]}" file:')
+        if len(case) > 1:
+            print(run_python_file("calculator", case[0], case[1]))
+        print(run_python_file("calculator", case[0]))
+
 
 def main():
     # test_get_file_info()
     # test_get_file_contents()
-    test_write_file()
+    # test_write_file()
+    test_run_python_file()
 
 
 main()
