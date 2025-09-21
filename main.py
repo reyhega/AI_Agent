f651b734f5dc10f8f615c5f21539d95e5f4d0162
reyhega <reyhega@gmail.com>
2025-09-20 20:07:20 -0600

    CH3 - L1: System Prompt

--- main.py
+++ main.py
@@ -7,19 +7,22 @@ from google.genai import types
 load_dotenv()
 
 api_key = os.environ.get("GEMINI_API_KEY")
+model_name = "gemini-2.0-flash-001"
 client = genai.Client(api_key=api_key)
 user_prompt = sys.argv[1]
 messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
 verbose_flag = len(sys.argv) > 2 and sys.argv[2] == "--verbose"
+system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
 
 if not sys.argv[1]:
     print("Please provide prompt")
     sys.exit(1)
 response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
+    model=model_name,
     contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt),
 )
 
 print(response.text)
